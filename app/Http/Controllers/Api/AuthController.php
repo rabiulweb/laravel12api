31875e380f8e8cb22a4aa32311c8ffc610de7046
Rabiul Islam <rabibd.it@gmail.com>
2025-11-13 00:24:41 +0600

    auth added

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,11 +1,89 @@
 <?php
-
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'                  => 'required|min:2',
+            'email'                 => 'required|email|unique:users,email',
+            'password'              => 'required|min:8|confirmed',
+            'password_confirmation' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'  => 'fail',
+                'message' => $validator->errors(),
+            ], 400);
+        }
+
+        $data = $request->all();
+        User::create($data);
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'User created successfully',
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'  => 'fail',
+                'message' => $validator->errors(),
+            ], 400);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+
+            $response['token'] = $user->createToken('BlogApp')->plainTextToken;
+            $response['email'] = $user->email;
+            $response['name']  = $user->name;
+
+            return response()->json([
+                'status'  => 'success',
+                'message' => "Logged in successfully!",
+                'data'    => $response,
+            ], 200);
+        } else {
+            return response()->json([
+                'status'  => 'fail',
+                'message' => 'Invalid creanditials',
+            ], 400);
+        }
+    }
+
+    public function profile()
+    {
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'data'   => $user,
+        ], 200);
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+        $user->tokens()->delete();
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'Logout successfully!',
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\StudentApiController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,11 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::apiResource('/students', StudentApiController::class);
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/profile', [AuthController::class, 'profile'])->name('profile');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
